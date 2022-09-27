c658c61ff3956875554e33a76d8dfbac99f4203c
vipos89 <1989146ViPoS>
2022-09-27 22:01:24 +0300

    lesson2

--- public/index.php
+++ public/index.php
@@ -1,27 +1,33 @@
 <?php
 
-    $arr2 = [1,2,3,4,5];
+    /**
+     * @var array $pages
+     */
 
-    $arr2['dddd'] = 111;
-    $arr2 = array_merge($arr2, [555, 556, 557, 558]);
-    
-    array_push($arr2, 555, 556, 557, 558);
-
-    print_r($arr2);
-
-    echo "git second branch";
+    require __DIR__.'/../src/pages.php';
 
 
+?>
 
-
-
-
-
-
-
-
-
-
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php foreach ($pages as $page):?>
+        <div class="post">
+            <h2><?=$page['title']?></h2>
+            <p><?=$page['content']?></p>
+            <a href="#">Link</a>
+        </div>
+    <?php endforeach; ?>
+</body>
+</html>
 
 
 
---
+++ src/pages.php
@@ -0,0 +1,21 @@
+<?php
+    $pages = [
+        [
+            'title' => 'ololo',
+            'content' => 'sdfsdfsdfsdf'
+        ],
+        [
+            'title' => 'ololo2',
+            'content' => 'sdfsdfsdfsdf'
+        ],
+        [
+            'title' => 'ololo3',
+            'content' => 'sdfsdfsdfsdf'
+        ],
+        [
+            'title' => 'ololo4',
+            'content' => 'sdfsdfsdfsdf'
+        ],
+    ];
+
+    echo "HELLO WORLD";
